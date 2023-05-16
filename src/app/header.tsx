101d948712993e2154d36013963f963af3d0a624
hiroto-hash <hiroto5118@icloud.com>
2023-05-16 17:50:57 +0900

    create header template

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,7 +1,25 @@
 export default function Header() {
   return (
-    <header>
-      <h1 className="text-5xl font-size: 3rem">Wear Create</h1>
+    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
+      <nav
+        className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
+        aria-label="Global"
+      >
+        <a className="flex-none text-5xl font-size:3rem" href="#">
+          Brand
+        </a>
+        <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:pl-5">
+          <a className="font-medium text-blue-500" href="#" aria-current="page">
+            hiroto
+          </a>
+          <a
+            className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+            href="#"
+          >
+            Logout
+          </a>
+        </div>
+      </nav>
     </header>
   );
 }
