d3c150a892868c5071f3c9cfa64229b9fd6430d3
hiroto-hash <hiroto5118@icloud.com>
2023-05-18 17:22:53 +0900

    create WC logo and footer layout

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
     <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
@@ -5,7 +7,17 @@ export default function Header() {
         className="max-w-full w-full mx-auto px-5 sm:flex sm:items-center sm:justify-between"
         aria-label="Global"
       >
-        <a className="text-gray-50 flex-none text-5xl font-size:3rem" href="#">
+        <a
+          className="text-gray-50 flex-none text-5xl font-size:3rem flex"
+          href="#"
+        >
+          <Image
+            className="rounded-t-lg"
+            src="/wc.svg"
+            alt=""
+            width={50}
+            height={50}
+          />
           Wear Create
         </a>
         <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:pl-5">
