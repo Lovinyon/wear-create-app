0cf640deaa3d68bb0a16e95614c539bf577e5689
hiroto-hash <hiroto5118@icloud.com>
2023-05-18 16:07:26 +0900

    update all background color

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,20 +2,21 @@ export default function Header() {
   return (
     <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
       <nav
-        className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
+        className="max-w-full w-full mx-auto px-5 sm:flex sm:items-center sm:justify-between"
         aria-label="Global"
       >
-        <a className="flex-none text-5xl font-size:3rem" href="#">
+        <a className="text-gray-50 flex-none text-5xl font-size:3rem" href="#">
           Wear Create
         </a>
         <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:pl-5">
-          <a className="font-medium text-blue-500" href="#" aria-current="page">
+          <a
+            className="font-medium text-gray-50 hover:text-blue-500"
+            href="#"
+            aria-current="page"
+          >
             hiroto
           </a>
-          <a
-            className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
-            href="#"
-          >
+          <a className="font-medium text-gray-50 hover:text-blue-500" href="#">
             Logout
           </a>
         </div>
