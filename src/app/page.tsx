e9c690ca19af77f1071d2a84e6510415ccd999c3
hiroto-hash <hiroto5118@icloud.com>
2023-05-21 22:01:16 +0900

    create plus button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Products from "./compnents/products";
+import Plus from "./compnents/product/plus";
 
 export default function Home() {
   return (
     <div>
       <Products />
+      <Plus />
     </div>
   );
 }
