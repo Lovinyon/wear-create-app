c08dadc7d0ee678accca9cee0c9260e97d4f23e7
hiroto-hash <hiroto5118@icloud.com>
2023-05-24 20:57:41 +0900

    update create button

--- src/app/compnents/product/product.tsx
+++ src/app/compnents/product/product.tsx
@@ -1,24 +1,19 @@
 import autoprefixer from "autoprefixer";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Product() {
   return (
-    <div className="max-w-sm rounded-lg dark:bg-gray-800 justify-self-center">
-      <Image
-        className="rounded-t-lg"
-        src="/black-t 1.svg"
-        alt=""
-        width={200}
-        height={200}
-      />
-      <div className="p-5 w-64 items-center">
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-400 rounded-lg hover:bg-purple-400 focus:ring-4 focus:outline-none focus:ring-purple-400 dark:bg-purple-400 dark:hover:bg-purple-500 dark:focus:ring-purple-500"
-        >
-          Edit
-        </a>
-      </div>
+    <div className="max-w-sm rounded-lg dark:bg-gray-800 justify-self-center dark:hover:bg-indigo-500/75">
+      <Link href="#">
+        <Image
+          className="rounded-t-lg hover:fill-cyan-700"
+          src="/black-t 1.svg"
+          alt=""
+          width={200}
+          height={200}
+        />
+      </Link>
     </div>
   );
 }
