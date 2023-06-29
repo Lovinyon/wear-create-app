f31f7548451d8c25c2dd7213e9b7cadeb8bef8b7
hiroto-hash <hiroto5118@icloud.com>
2023-06-29 18:02:49 +0900

    wordpress get article start

--- src/app/compnents/products.tsx
+++ src/app/compnents/products.tsx
@@ -1,13 +1,23 @@
 import Product from "./product/product";
 
+// type Article = {
+//   id: string;
+//   title: string;
+// };
+
+async function getData() {
+  const response = await fetch(
+    "https://lovinyon.com/wp-json/wp/v2/posts?_embed"
+  );
+  const articles = await response.json();
+  console.log(articles);
+}
+
 export default function Products() {
+  const data = getData();
   return (
     <div className="grid primitive grid-cols-5 gap-4 items-stretch">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      <ul></ul>
     </div>
   );
 }
